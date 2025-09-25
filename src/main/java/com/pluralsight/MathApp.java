895146f03b1cc2388ffa66013e7e18e62432c029
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 12:03:19 -0500

    lines five through fourteen commit

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        // Step 1
+        // Question 1
+        // Create two variables for Bob and Gary's salary
+        double bobSalary = 80_000.00;
+        double garySalary = 100_000.00;
+        // Create a new variable named highestSalary using Math.max()
+        // store the answer in highestSalary
+        double highestSalary = Math.max(bobSalary, garySalary);
+        // Print the answer "The highest salary is..."
+        System.out.println("The highest salary is " + highestSalary);
+    }
 }
