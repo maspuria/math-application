e347df02c6875053dd56d62a5e324a18d9f65fe5
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 13:19:17 -0500

    question seven commit

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -31,6 +31,34 @@ public class MathApp {
         double squareRoot = Math.sqrt(num);
         System.out.println("The square root of 5.0 is " + squareRoot);
         // Question 5
+        // Find and display the distance between two points (5,10) and (85,50)
+        double a1 = 5;
+        double b1 = 10;
+        double a2 = 85;
+        double b2 = 50;
+        // to find the square root use Math.sqrt
+        // to find the power of a number use Math.pow(x,2)
+        double distance = Math.sqrt(Math.pow(a2-a1,2) + Math.pow(b2-b1,2));
+        System.out.println("The distance between (5,10) and (85,50) is " + distance);
+        // Question 6
+        // Find the absolute (positive) value of a variable after it is set to -3.8
+        // identify the variable
+        double number = -3.8;
+        // use Math.abs to find the absolute value of a number
+        double absoluteValue = Math.abs(number);
+        // solution
+        System.out.println("The absolute value of -3.8 is " +  absoluteValue);
+        // Question 7
+        // Find a random number between 0 and 1
+        // use Math.random() because it randomizes a number between 0 and 1
+        // if you wanted to extend the number 1 to any number then you would multiply by that number
+        // Ex: Math.random() * anyNumber
+        double randomNumber = Math.random();
+        System.out.println("A random number between 0 and 1 is " + randomNumber);
+
+
+
+
 
     }
 }
